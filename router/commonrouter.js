17f46563258ea87597f2b28b83725b8b2831357e
kabilan-7-v <kabilanvelmani@gmail.com>
2025-03-31 19:52:24 +0530

    previous

--- router/commonrouter.js
+++ router/commonrouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Logincreate, Loginverify } = require('../Controller/Login.controller');
 const { addmenucard, getmenucards, deletemenucard } = require('../Controller/addmenucard.controller');
 const { AddPlace, updatePlace, getPlaces } = require('../Controller/addplace.controller'); // ✅ Import updatePlace
-const { addFeedback, updateFeedbackIsButton,  } = require('../Controller/feedback.controller');
+const { addFeedback, updateFeedbackIsButton, } = require('../Controller/feedback.controller');
 
 const route = express.Router();
 
@@ -11,9 +11,9 @@ route.post("/loginverify", Loginverify);
 route.post("/addmenucard", addmenucard);
 route.post("/addplace", AddPlace);
 route.put("/updateplace/:id", updatePlace); // ✅ Added updatePlace route
-route.post("/addfeedback",addFeedback)
-route.get("/getplace",getPlaces)
-route.get("/getmenucard",getmenucards)
+route.post("/addfeedback", addFeedback)
+route.get("/getplace", getPlaces)
+route.get("/getmenucard", getmenucards)
 route.put("/feedback/:id", updateFeedbackIsButton);
 route.delete("/deletemenucard/:id", deletemenucard);
 
